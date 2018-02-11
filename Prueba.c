d3dc9519172194b8df6cde2b3280b2052bbd6a89
Roberto Herrera <prometeofarfare01@hotmail.com>
2018-02-11 02:00:41 -0600

    Prueba de envío.
    A continuación se realiza la prueba de enviar un archivo hacia la rama 1.

---
+++ Prueba.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main()
+{
+	int ano_A=2018, ano_N=1998, edad=0;
+	edad=ano_A - ano_N;
+	printf("Tu edad es de: %d anos\n",edad);
+
+	system("PAUSE");
+}
